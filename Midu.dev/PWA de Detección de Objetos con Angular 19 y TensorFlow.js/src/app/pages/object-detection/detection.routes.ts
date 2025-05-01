6b3f7542b533528eac8c993b43bee3105a7d9126
Número Cuatro <luigi@cuatro.dev>
2025-04-30 23:51:26 -0400

    PWA de Detección de Objetos con Angular 19 y TensorFlow.js (#7)

---
+++ Midu.dev/PWA de Detección de Objetos con Angular 19 y TensorFlow.js/src/app/pages/object-detection/detection.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+
+const detectionRoutes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./object-detection.component').then(
+        (c) => c.ObjectDetectionComponent
+      ),
+  },
+];
+
+export default detectionRoutes;

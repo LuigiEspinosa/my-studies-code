5cbd58373f6f57b2f553157463553f7264f29a34
Número Cuatro <luigi@cuatro.dev>
2025-06-10 07:47:20 -0500

    feat: Phaser No-Internet Dino

---
+++ Udemy/Game Development in JS-TS with Phaser 3/no-internet-dino/src/index.ts
@@ -0,0 +1,23 @@
+import Phaser from "phaser";
+import PreloadScene from "./scenes/PreloadScene";
+import PlayScene from "./scenes/PlayScene";
+
+export const PRELOAD_CONFIG = {
+	cactusesCount: 6,
+	birdsCount: 1,
+};
+
+const config: Phaser.Types.Core.GameConfig = {
+	type: Phaser.AUTO,
+	width: 1000,
+	height: 340,
+	pixelArt: true,
+	transparent: true,
+	physics: {
+		default: "arcade",
+	},
+	scene: [PreloadScene, PlayScene],
+};
+
+new Phaser.Game(config);
+
